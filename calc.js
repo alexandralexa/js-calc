3521ea6f43172884c228131d7693fd388d1ea91b
alexandraalexa <gubanovasasa@gmail.com>
2021-09-22 21:38:14 +0300

    edit

---
+++ calc.js
@@ -0,0 +1,23 @@
+let a=document.getElementById(`fnumb`);
+   let b=document.getElementById(`lnumb`);
+
+   function sum(){
+     alert(+a+ + b);
+   }
+
+   
+
+   function min(){
+     alert(+a - +b);
+   }
+
+   function multy(){
+     alert(+a * +b)
+   }
+
+   function division(){
+     alert(+a / +b)
+     
+   }
+
+   showMessage2();
